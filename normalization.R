23b56f218532114ed4837175695b35ec7c05d412
Meghana <meghana.paranjape@usherbrooke.ca>
2022-08-03 16:36:12 -0400

    remove NoDATA
    Updata normalization script to remove NoDATA from attribute table and get correct min max values for normalising.

--- normalization.R
+++ normalization.R
@@ -1,6 +1,9 @@
 #This script is for normalizing the metrics
-
+install.packages('naniar')
+install.packages('tidyr')
 library(sf)
+library(naniar)
+library(tidyr)
 #If UREC_merge and UERC_norm already exists then open from the correct location 
 UREC_merge = st_read('C:/Meghana/Belgique/traitements/results/UREC_merge/UREC_merge_new.shp')
 #If UREC_norm exists : 
@@ -9,8 +12,8 @@ UREC_norm = st_read('C:/Meghana/Belgique/traitements/results/UREC_merge/UREC_mer
 ################################################################################
 #This code is to check that each UREC_file has the same number of columns 
 i =2
-for (i in 1:length(list_files_UREC_rives)){
-  test = st_read(list_files_UREC_rives[i])
+for (i in 1:length(list_files_UREC_rives_TBD)){ #TO be changed
+  test = st_read(list_files_UREC_rives_TBD[i])  #TO be changed
   if (i ==1){
     check = test
     cnt_col_check = ncol(check)}
@@ -19,6 +22,10 @@ for (i in 1:length(list_files_UREC_rives)){
     if (cnt_col != cnt_col_check){
       print(paste0('The number of columns in ', test$Id_UEA, ' rive ', test$Id_rive, ' is: ', cnt_col ))
       print(paste0('The number of columns in check is : ', cnt_col_check))
+      print('The number of columns does not match previous feature')
+    } else {
+      print('The number of columns match.')
+      print(paste0('iteration ', i))
     }
   }
 }
@@ -26,37 +33,40 @@ for (i in 1:length(list_files_UREC_rives)){
 #This script is to merge all UREC_files into one. This may not be necessary if the UREC_merge file already exists. 
 #merge all the UREC_rive shapefiles with pre-calculated metrics. ATTENTION : may need to change save file name
 i = 2
-for (i in 1:length(list_files_UREC_rives)) 
+#Initilize variable UREC_merge outside of for loop
+UREC_merge = st_read(list_files_UREC_rives_TBD[1])
+i = 2
+for (i in 2:length(list_files_UREC_rives_TBD))
+  #Start for loop at posiion 2 because of initialization process
 {
-  shp = st_read(list_files_UREC_rives[i])
+  shp = st_read(list_files_UREC_rives_TBD[i])
   print(paste0('reading UEA file ', shp$Id_UEA, ' at rive ', shp$Id_rive))
-  if (i == 1) 
-  {
-    UREC_merge = shp
-  } 
-  else 
-  {
-    print(paste0('binding UREC_merge with UEA ', shp$Id_UEA, ' at rive ', shp$Id_rive))
-    names = names(UREC_merge)
-    print(names)
-    names_shp = names(shp)
-    UREC_merge = rbind(UREC_merge, shp) 
-    st_write(UREC_merge, 'C:/Meghana/Belgique/traitements/results/UREC_merge/UREC_merge_new.shp', delete_layer = T)
-    st_write(UREC_merge, 'C:/Meghana/Belgique/traitements/results/UREC_merge/UREC_merge_new.SQLite', delete_layer = T) #write the file in sqlite format because it doesn't abbreviate column names
-    
-  }
+  print(paste0(
+    'binding UREC_merge with UEA ',
+    shp$Id_UEA,
+    ' at rive ',
+    shp$Id_rive
+  ))
+  names = names(UREC_merge)
+  print(names)
+  names_shp = names(shp)
+  UREC_merge = rbind(UREC_merge, shp)
+  print(paste0('UREC_merge has been updtated ', i, ' times'))
+  st_write(UREC_merge, 'C:/Meghana/Belgique/traitements/results/UREC_merge/UREC_merge_new.shp', delete_layer = T)
+  st_write(UREC_merge, 'C:/Meghana/Belgique/traitements/results/UREC_merge/UREC_merge_new.SQLite', delete_layer = T) #write the file in sqlite format because it doesn't abbreviate column names
 }
 ################################################################################
 
 
 #Normalizing script for UREC_merge
 #ELse : Script to Initiate normalizing of metrics with min max 
-UREC_norm = UREC_merge #create normaliation table
+UREC_norm = UREC_merge #create normalization table
 UREC_norm = st_cast(UREC_norm, to = 'MULTIPOLYGON')
 UREC_norm$ext_geometry = st_geometry(UREC_norm) #Extract geometry for  into a new column
 UREC_norm = st_drop_geometry(UREC_norm) #Drop geometry (drops geometry column )
+#UREC_norm = naniar::replace_with_na(data = UREC_norm, replace = c(NA))
 #Do normalization with min max values by iterating through sf dataframe columns 
-f=3
+f=11
 for (f in colnames(UREC_norm)) {
   feature_table = select(UREC_norm, all_of(f)) #select one column
   colname = names(feature_table)
@@ -67,15 +77,20 @@ for (f in colnames(UREC_norm)) {
     print('Colename is not Id_UEA or Id_rive or geometry.')
     new_colname =   paste0(names(feature_table), '_nrm')#Create name for normalized column
     print(new_colname) #Print new column name to make sure it looks correct
-    max_val = max(feature_table) #find maximum value in column and put it in a variable
+    feature_table_clean = subset(feature_table, feature_table[,1]>-999)
+    max_val = max(feature_table_clean, na.rm = T) #find maximum value in column and put it in a variable
     print(max_val)
-    min_val = min(feature_table) #find minimum value in column and put it in a variable
+    min_val = min(feature_table_clean, na.rm = T) #find minimum value in column and put it in a variable
     print(min_val)
     #iterate though each row of the column we are looking at (f)
     for (n in 1:nrow(UREC_norm)) {
-      normalisation = (feature_table - min_val) / (max_val - min_val) #Min max normalisation equation
-      #Create a new column with new column name and add normalisation value for the row. (This may not be optimal)
-      UREC_norm[, new_colname] <- normalisation 
+      if(UREC_norm[n,colname] == -999){
+        UREC_norm[n,new_colname] = -999
+      }
+      else {
+        normalisation = (feature_table[n,1] - min_val) / (max_val - min_val) #Min max normalisation equation
+        #Create a new column with new column name and add normalisation value for the row. (This may not be optimal)
+        UREC_norm[n, new_colname] <- normalisation}
     }
   }
 }
